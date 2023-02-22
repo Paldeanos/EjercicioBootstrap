7bca8d906a839758fe41315ef00cee5b92975b60
agonzalezrando <93986162+AlvaroCesur@users.noreply.github.com>
2023-02-22 17:09:37 +0100

    Revert "Prueba"
    This reverts commit 49e973203670a98496f9089abd13eb41affba5f9.

--- js/app.js
+++ js/app.js
@@ -93,7 +93,7 @@ function cargarDatos() {
 
     })
       .catch(err => {
-        alert("Hubo un error" + err +". Recargue la página.")
+        alert("Hubo error. Recargue la página.")
       });
   });
 };
@@ -113,7 +113,6 @@ botonInsert.addEventListener("click", () => {
   // Loop over them and prevent submission
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
-      // Se valida el formulario antes de enviar los datos
       if (!form.checkValidity()) {
         event.preventDefault()
         event.stopPropagation()
@@ -191,16 +190,24 @@ botonInsert.addEventListener("click", () => {
         celdaGER.textContent = calculoGER;
         celdaGET.textContent = metabolismoGET(actividadCliente, calculoGER, sexoCliente);
 
-        // // Limpiar el formulario y cerrar el modal
-        // document.getElementById("formulario").reset();
-        // let modal = bootstrap.Modal.getInstance(
-        //   document.getElementById("staticBackdrop")
-        // );
-        // modal.hide();
+        // Limpiar el formulario y cerrar el modal
+        document.getElementById("formulario").reset();
+        let modal = bootstrap.Modal.getInstance(
+          document.getElementById("staticBackdrop")
+        );
+        modal.hide();
       }
 
-      form.classList.add('was-validated');
+      form.classList.add('was-validated')
     }, false)
   })
 
-})
+})()
+
+  /* Modal */
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+});
