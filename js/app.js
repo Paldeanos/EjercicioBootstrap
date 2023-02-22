6d307a764d23a5e071863913c5b199a2fec04988
agonzalezrando <93986162+AlvaroCesur@users.noreply.github.com>
2023-02-22 18:39:01 +0100

    Insercion con Template

--- js/app.js
+++ js/app.js
@@ -1,102 +1,74 @@
 'use strict'
 
+let plantilla = document.querySelector("template"); 
+let contenedor = document.querySelector("tbody");
+
   /* Importación de datos */
 function cargarDatos() {
   fetch("https://raw.githubusercontent.com/FRomero999/ExamenDIW2022/main/clientes.json")
   .then(response => response.json())
-  .then(data => {
+    .then(data => {
+      data.forEach(function (el) {
 
-    data.forEach(function (el) {
+        let wrap = document.createElement("tr");
 
-      let tabla = document.getElementById("tabla");
-      let tbody = tabla.getElementsByTagName("tbody")[0];
+        let nuevoCliente = plantilla.content.cloneNode(true);
+        
+        nuevoCliente.querySelector(".celdaNombre").textContent = el.nombre;
+        nuevoCliente.querySelector(".celdaApellidos").textContent = el.apellidos;
+        nuevoCliente.querySelector(".spanSexo").textContent = el.sexo;
+        nuevoCliente.querySelector(".celdaEdad").textContent = el.edad;
+        nuevoCliente.querySelector(".celdaAltura").textContent = el.altura;
+        nuevoCliente.querySelector(".celdaPeso").textContent = el.peso;
+        nuevoCliente.querySelector(".spanActividad").textContent = el.actividad;
 
-      let fila = document.createElement("tr");
+        // Se crea una funcion para calcular el metabolismo GER
+        const metabolismoGER = function (peso, altura, edad, sexo) {
+          if (sexo === 'hombre') {
+            return Math.floor(66.473 + (13.751 * peso) + (5.0033 * altura) - (6.755 * edad));
+          } else if (sexo === 'mujer') {
+            return Math.floor(655.0955 + (9.463 * peso) + (1.8496 * altura) - (4.6756 * edad));
+          } else {
+            return "Error en el cálculo";
+          }
+        };
 
-      let celdaNombre = document.createElement("td");
-      let celdaApellidos = document.createElement("td");
-      let celdaSexo = document.createElement("td");
-      let celdaEdad = document.createElement("td");
-      let celdaPeso = document.createElement("td");
-      let celdaAltura = document.createElement("td");
-      let celdaActividad = document.createElement("td");
-      let celdaGET = document.createElement("td");
-      let celdaGER = document.createElement("td");
+        // Se crea una funcion para calcular el metabolismo GET
+        const metabolismoGET = function (nivelActividad, metabolismoGER, sexo) {
+          if (nivelActividad === "sedentaria" && sexo === 'hombre') {
+            return Math.floor(metabolismoGER * 1.3);
+          } else if (nivelActividad === "ligera" && sexo === 'hombre') {
+            return Math.floor(metabolismoGER * 1.6);
+          } else if (nivelActividad === "moderada" && sexo === 'hombre') {
+            return Math.floor(metabolismoGER * 1.7);
+          } else if (nivelActividad === "intensa" && sexo === 'hombre') {
+            return Math.floor(metabolismoGER * 2.1);
+          } else if (nivelActividad === "sedentaria" && sexo === 'mujer') {
+            return Math.floor(metabolismoGER * 1.3);
+          } else if (nivelActividad === "ligera" && sexo === 'mujer') {
+            return Math.floor(metabolismoGER * 1.5);
+          } else if (nivelActividad === "moderada" && sexo === 'mujer') {
+            return Math.floor(metabolismoGER * 1.6);
+          } else if (nivelActividad === "intensa" && sexo === 'mujer') {
+            return Math.floor(metabolismoGER * 1.9);
+          } else {
+            return "Error en el cálculo";
+          }
+        };
 
-      let spanSexo = document.createElement("span");
-      let spanActividad = document.createElement("span");
+        const calculoGER = metabolismoGER(el.peso, el.altura, el.edad, el.sexo);
+        nuevoCliente.querySelector(".celdaGET").textContent = metabolismoGET(el.actividad, calculoGER, el.sexo);
+        nuevoCliente.querySelector(".celdaGER").textContent = calculoGER;
 
-      // Se crea una funcion para calcular el metabolismo GER
-      const metabolismoGER = function (peso, altura, edad, sexo) {
-        if (sexo === 'hombre') {
-          return Math.floor(66.473 + (13.751 * peso) + (5.0033 * altura) - (6.755 * edad));
-        } else if (sexo === 'mujer') {
-          return Math.floor(655.0955 + (9.463 * peso) + (1.8496 * altura) - (4.6756 * edad));
-        } else {
-          return "Error en el cálculo";
-        }
-      };
+        wrap.appendChild(nuevoCliente);       
+        contenedor.appendChild(wrap);
 
-      // Se crea una funcion para calcular el metabolismo GET
-      const metabolismoGET = function (nivelActividad, metabolismoGER, sexo) {
-        if (nivelActividad === "sedentaria" && sexo === 'hombre') {
-          return Math.floor(metabolismoGER * 1.3);
-        } else if (nivelActividad === "ligera" && sexo === 'hombre'){
-          return Math.floor(metabolismoGER * 1.6);
-        } if (nivelActividad === "moderada" && sexo === 'hombre') {
-          return Math.floor(metabolismoGER * 1.7);
-        } else if (nivelActividad === "intensa" && sexo === 'hombre') {
-          return Math.floor(metabolismoGER * 2.1);
-        } else if (nivelActividad === "sedentaria" && sexo === 'mujer') {
-          return Math.floor(metabolismoGER * 1.3);
-        } else if (nivelActividad === "ligera" && sexo === 'mujer') {
-          return Math.floor(metabolismoGER * 1.5);
-        } else if (nivelActividad === "moderada" && sexo === 'mujer') {
-          return Math.floor(metabolismoGER * 1.6);
-        } else if (nivelActividad === "intensa" && sexo === 'mujer') {
-          return Math.floor(metabolismoGER * 1.9);
-        } else {
-          return "Error en el cálculo";
-        }
-      }
-
-      celdaNombre.textContent = el.nombre
-      celdaApellidos.textContent = el.apellidos
-      celdaEdad.textContent = el.edad
-      celdaPeso.textContent = el.peso
-      celdaAltura.textContent = el.altura
-
-      spanSexo.textContent = el.sexo
-      spanActividad.textContent = el.actividad
-      const calculoGER = metabolismoGER(el.peso, el.altura, el.edad, el.sexo);
-      celdaGER.textContent = calculoGER;
-      celdaGET.textContent = metabolismoGET(el.actividad, calculoGER, el.sexo);
-
-      spanSexo.classList.add("bg-primary", "rounded", "text-white", "px-2");
-      celdaSexo.appendChild(spanSexo);
-
-      spanActividad.classList.add("bg-secondary", "rounded", "text-white", "px-2");
-      celdaActividad.appendChild(spanActividad);
-
-
-      fila.appendChild(celdaNombre);
-      fila.appendChild(celdaApellidos);
-      fila.appendChild(celdaSexo);
-      fila.appendChild(celdaEdad);
-      fila.appendChild(celdaPeso);
-      fila.appendChild(celdaAltura);
-      fila.appendChild(celdaActividad);
-      fila.appendChild(celdaGER);
-      fila.appendChild(celdaGET);
-      tbody.appendChild(fila);
-
-
-    })
-      .catch(err => {
-        alert("Hubo error. Recargue la página.")
       });
-  });
-};
+    })
+    .catch(err => {
+        alert("Hubo un error: " + err +". Recargue la página.");
+      });
+    };
 
 document.addEventListener("DOMContentLoaded", cargarDatos);
 
@@ -114,12 +86,9 @@ botonInsert.addEventListener("click", () => {
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
       if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
+        event.preventDefault();
+        event.stopPropagation();
       } else {
-        // Se selecciona la tabla ya creada
-        const datosCargados = document.querySelector("tbody");
-
         // Se obtienen los datos del formulario
         const nombreCliente = document.querySelector("#nombre").value;
         const apellidosCliente = document.querySelector("#apellidos").value;
@@ -129,19 +98,17 @@ botonInsert.addEventListener("click", () => {
         const pesoCliente = document.querySelector("#peso").value;
         const alturaCliente = document.querySelector("#altura").value;
 
-        // Se inserta la nueva fila al final
-        let fila = datosCargados.insertRow(-1);
+        let wrap = document.createElement("tr");
 
-        // Se insertan las celdas una detrás de otra
-        let celdaNombre = fila.insertCell(0);
-        let celdaApellidos = fila.insertCell(1);
-        let celdaSexo = fila.insertCell(2);
-        let celdaEdad = fila.insertCell(3);
-        let celdaAltura = fila.insertCell(4);
-        let celdaPeso = fila.insertCell(5);
-        let celdaActividad = fila.insertCell(6);
-        let celdaGET = fila.insertCell(7);
-        let celdaGER = fila.insertCell(8);
+        let nuevoCliente = plantilla.content.cloneNode(true);
+        
+        nuevoCliente.querySelector(".celdaNombre").textContent = nombreCliente;
+        nuevoCliente.querySelector(".celdaApellidos").textContent = apellidosCliente;
+        nuevoCliente.querySelector(".spanSexo").textContent = sexoCliente;
+        nuevoCliente.querySelector(".celdaEdad").textContent = edadCliente;
+        nuevoCliente.querySelector(".celdaAltura").textContent = alturaCliente;
+        nuevoCliente.querySelector(".celdaPeso").textContent = pesoCliente;
+        nuevoCliente.querySelector(".spanActividad").textContent = actividadCliente;
 
         // Se crea una funcion para calcular el metabolismo GER
         const metabolismoGER = function (peso, altura, edad, sexo) {
@@ -150,7 +117,7 @@ botonInsert.addEventListener("click", () => {
           } else if (sexo === 'mujer') {
             return Math.floor(655.0955 + (9.463 * peso) + (1.8496 * altura) - (4.6756 * edad));
           } else {
-            return "Error en el cálculo.";
+            return "Error en el cálculo";
           }
         };
 
@@ -158,9 +125,9 @@ botonInsert.addEventListener("click", () => {
         const metabolismoGET = function (nivelActividad, metabolismoGER, sexo) {
           if (nivelActividad === "sedentaria" && sexo === 'hombre') {
             return Math.floor(metabolismoGER * 1.3);
-          } else if (nivelActividad === "ligera" && sexo === 'hombre'){
+          } else if (nivelActividad === "ligera" && sexo === 'hombre') {
             return Math.floor(metabolismoGER * 1.6);
-          } if (nivelActividad === "moderada" && sexo === 'hombre') {
+          } else if (nivelActividad === "moderada" && sexo === 'hombre') {
             return Math.floor(metabolismoGER * 1.7);
           } else if (nivelActividad === "intensa" && sexo === 'hombre') {
             return Math.floor(metabolismoGER * 2.1);
@@ -173,22 +140,16 @@ botonInsert.addEventListener("click", () => {
           } else if (nivelActividad === "intensa" && sexo === 'mujer') {
             return Math.floor(metabolismoGER * 1.9);
           } else {
-            return "Error en el cálculo.";
+            return "Error en el cálculo";
           }
         };
-
-        // Se rellenan las celdas con los datos del formulario
-        celdaNombre.textContent = nombreCliente;
-        celdaApellidos.textContent = apellidosCliente;
-        celdaSexo.textContent = sexoCliente;
-        celdaActividad.textContent = actividadCliente;
-        celdaEdad.textContent = edadCliente;
-        celdaPeso.textContent = pesoCliente;
-        celdaAltura.textContent = alturaCliente;
         
         const calculoGER = metabolismoGER(pesoCliente, alturaCliente, edadCliente, sexoCliente);
-        celdaGER.textContent = calculoGER;
-        celdaGET.textContent = metabolismoGET(actividadCliente, calculoGER, sexoCliente);
+        nuevoCliente.querySelector(".celdaGET").textContent = calculoGER;
+        nuevoCliente.querySelector(".celdaGER").textContent = metabolismoGET(actividadCliente, calculoGER, sexoCliente);
+
+        wrap.appendChild(nuevoCliente);       
+        contenedor.appendChild(wrap);
 
         // Limpiar el formulario y cerrar el modal
         document.getElementById("formulario").reset();
@@ -199,15 +160,7 @@ botonInsert.addEventListener("click", () => {
       }
 
       form.classList.add('was-validated')
-    }, false)
-  })
+    }, false);
+  });
 
-})()
-
-  /* Modal */
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
 });
